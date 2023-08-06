d875faf55e12ea35e25159d0f6b8784c9dbf7a73
HumbleG0d <sergiouni2000@gmail.com>
2023-08-06 15:07:10 -0500

    add the respective functions to add and order the numbers these are used in their respective components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,49 @@
 import './App.css'
+import './css/addTodo.css'
+import './css/square.css'
+import { AddTodo } from './components/AddTodo'
+import { Button } from './components/Button'
+import { useState } from 'react'
+import { Square } from './components/Square'
+import { quickSort } from './js/quick_sort.js'
 
 function App() {
 
+  const [arr , setArr] = useState([])
+
+  const addNumer = (n) => {
+    setArr((prevArr) => [
+      ...prevArr,
+      {id : prevArr.length , value : n },
+    ])
+  }
+
+  const sortArray = (orderType) => {
+    const sortArr = [...arr]
+    quickSort(sortArr , 0 , sortArr.length - 1 , orderType)
+    setArr(sortArr)
+  }
+
   return (
     <>
-    
+      <section className='bar'>
+        <AddTodo addNumber={addNumer} />
+      </section>
+      <section className='buttons'>
+        <Button 
+          name={'Ascendente'}
+          handelFunction={() => {sortArray('ascendente')}}
+        />
+        <Button 
+          name={'Descendente'}
+          handelFunction={() => {sortArray('descendente')}}
+        />
+      </section>
+      <section className='list'>
+        {arr.map((n) => (
+          <Square key={n.id} number={n.value}/>
+        ))}
+      </section>
     </>
   )
 }
