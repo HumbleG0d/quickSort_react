cc554b8063bfa1040f7a7cd916cfbfded15d21b1
HumbleG0d <sergiouni2000@gmail.com>
2023-08-06 15:03:52 -0500

    add the add number prop plus the functionality for the input and the button

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,10 +1,34 @@
 import { Button } from "./Button";
+import { useState } from "react";
 
-export function AddTodo(){
+export function AddTodo({ addNumber }){
+
+  const [number , setNumber] = useState('')
+
+  const changeValue = (e) => {
+    const input_value = e.target.value
+    setNumber(input_value === '' ? '' : parseInt(input_value))
+  }
+
+  const handelClick = () => {
+    if(!isNaN(number)){
+      addNumber(number)
+    }
+    setNumber('')
+  }
+
   return (
     <>
-      <input type="text" className="input"/>
-      <Button name={'Agregar'}/>
+      <input 
+        type="text" 
+        className="input"
+        value={number === '' ? '' : number}
+        onChange={changeValue}
+      />
+      <Button 
+        name={'Agregar'}
+        handelFunction={handelClick}
+      />
     </>
   )
 }
